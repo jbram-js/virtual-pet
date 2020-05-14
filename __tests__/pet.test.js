e9c8e2a3bee75edeed345d80d1c232688e3ff218
“Joe <josephbramwell@bolstersystems.com>
2020-05-14 21:21:27 +0100

    worked through fitness, walking feeding

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -29,6 +29,74 @@ describe('growUp', () => {
       pet.growUp();
   
       expect(pet.age).toEqual(1);
+    });  
+    it('increments the hunger by 5', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.growUp();
+  
+      expect(pet.hunger).toEqual(5);
+    });  
+    it('decreases the fitness by 3', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.growUp();
+  
+      expect(pet.fitness).toEqual(7);
+    }); 
+  });
+
+  describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.fitness = 4;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(8);
+    });
+    it('increases fitness by 4 to a maximum of 10', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.fitness = 7;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(10);
+    });
+    it('increases fitness by 4 to a maximum of 10', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.fitness = 8;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(10);
+    });
+  });
+
+  describe('feed', () => {
+    it('decreases hunger by 3', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.hunger = 15;
+      pet.feed();
+  
+      expect(pet.hunger).toEqual(12);
+    });
+    it('decreases hunger by 3 to a minimum of 0', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.hunger = 2;
+      pet.feed();
+  
+      expect(pet.hunger).toEqual(0);
+    });
+    it('decreases hunger by 3 to a minimum of 0', () => {
+      const pet = new Pet('The puppy formerly known as Prince');
+  
+      pet.hunger = 1;
+      pet.feed();
+  
+      expect(pet.hunger).toEqual(0);
     });
   });
   
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,34 @@
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_AGE = 0;
+const MINIMUM_HUNGER = 0;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
   }
   
   Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
   };
+
+  Pet.prototype.walk = function() {
+    if (this.fitness <= 6 ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = 10;
+    }
+  }
+
+  Pet.prototype.feed = function() {
+    if (this.hunger >= 3 ) {
+      this.hunger -= 3;
+    } else {
+      this.hunger = 0;
+    }
+  }
 
 module.exports = Pet;
