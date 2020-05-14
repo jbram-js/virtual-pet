a2a6fc2b307b835a3821f907addda32bee8ea140
“Joe <josephbramwell@bolstersystems.com>
2020-05-14 17:42:37 +0100

    updated name

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,21 +2,21 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('The puppy formerly known as Prince')).toBeInstanceOf(Object);
     });
   });
 
   describe('name', () => {
     it('sets the name property', () => {
-      const pet = new Pet('Fido');
+      const pet = new Pet('The puppy formerly known as Prince');
   
-      expect(pet.name).toEqual('Fido');
+      expect(pet.name).toEqual('The puppy formerly known as Prince');
     });
   });
 
   describe('age', () => {
   it('has a initial age of 0', () => {
-    const pet = new Pet('Fido');
+    const pet = new Pet('The puppy formerly known as Prince');
 
     expect(pet.age).toEqual(0);
   });
@@ -24,7 +24,7 @@ describe('constructor', () => {
 
 describe('growUp', () => {
     it('increments the age by 1', () => {
-      const pet = new Pet('Fido');
+      const pet = new Pet('The puppy formerly known as Prince');
   
       pet.growUp();
   
